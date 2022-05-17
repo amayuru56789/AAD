db829e9851aac534b522c1cb4877a12ecb89f8ea
amayuru56789 <indeewara5678@gmail.com>
2022-05-17 12:21:48 +0530

    First Request called

--- JavaEE/01_HelloServlet/src/HelloServlet.java
+++ JavaEE/01_HelloServlet/src/HelloServlet.java
@@ -1,10 +1,24 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(/*name = "MyServlet",*/urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
     /*public static void main(String[] args) {
         System.out.println("Hello Tonny");
     }*/
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        /*super.doGet(req, resp);*/
+        System.out.println("Request is received...");
+        PrintWriter writer = resp.getWriter();
+        writer.write("Response Send");
+    }
 
 
 
