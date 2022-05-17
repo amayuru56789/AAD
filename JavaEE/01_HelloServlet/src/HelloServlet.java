43815ce627d8346dae138d861a6e4d0170c16611
amayuru56789 <indeewara5678@gmail.com>
2022-05-17 09:46:08 +0530

    Initial Commit

---
+++ JavaEE/01_HelloServlet/src/HelloServlet.java
@@ -0,0 +1,11 @@
+import javax.servlet.http.HttpServlet;
+
+public class HelloServlet extends HttpServlet {
+    /*public static void main(String[] args) {
+        System.out.println("Hello Tonny");
+    }*/
+
+
+
+
+}
